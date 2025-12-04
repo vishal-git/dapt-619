3fe701ddf87a0f284bc1f537c65e02659d08cd5d
Vishal <vishal@derive.io>
2025-12-04 17:09:38 -0500

    Added tutorial versions for OpenAI scripts

---
+++ src/openai_api_chart_tutorial.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import base64
+
+# step 1: load the API key
+# --
+
+# step 2: create the OpenAI client
+# --
+
+# step 3: create the response
+# --
+
+# step 4: print the response
+# --
---
+++ src/openai_api_text_tutorial.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# step 1: load the API key
+# --
+
+# step 2: create the OpenAI client
+# --
+
+# step 3: create the response
+# --
+
+# step 4: print the response
+# --
