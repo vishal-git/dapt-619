511c0ff56311500f4dbfe6a338757cc08e889c3b
Vishal <vishal@derive.io>
2025-12-04 17:07:30 -0500

    Added tutorial version of CIFAR API script

---
+++ src/cifar_api_tutorial.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+
+from utils import load_model, preprocess_image, predict_image, CIFAR10_CLASSES
+
+# step 1: load the model
+# --
+
+# step 2: create the FastAPI app
+# --
+
+# step 3: define the root endpoint
+# --
+
+# step 4: define the predict endpoint
+# --
+
+# step 5: run the API
+# --
